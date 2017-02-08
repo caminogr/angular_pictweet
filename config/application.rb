1d6a6119716bfa66674f89025eafa5ab6a3d5f29
Koki Kaminoura <amino.t02y@gmail.com>
2017-02-08 17:20:45 +0900

    make test file not to be created

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module AngularPictweet
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
