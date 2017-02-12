5540963caecb3a2f8ae5d28494bc1f42ffd2f5f8
Koki Kaminoura <amino.t02y@gmail.com>
2017-02-12 16:50:15 +0900

    use middleware for Flash

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module AngularPictweet
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
     config.generators do |g|
       g.test_framework false
     end
